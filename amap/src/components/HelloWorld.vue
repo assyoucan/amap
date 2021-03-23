b670e63cf97d61d104c6f14f31312fd5ca1939f2
chenmd <chen.mingda@cpe-smartcity.com>
2021-03-23 13:47:03 +0800

    增加矩形画布，圆形画布

--- amap/src/components/HelloWorld.vue
+++ amap/src/components/HelloWorld.vue
@@ -1,21 +1,98 @@
 <template>
-    <div>
-        123
+    <div class="amp-demo">
+        <el-amap vid="amap" :zoom="zoom" :zooms="zooms" :center="center" class="amap-demo" v-if="reFresh">
+            <!-- 点坐标  -->
+            <el-amap-marker v-for="item in markers" :position="item.position" :key="item.id"></el-amap-marker>
+            <!-- <el-amap-bezier-curve :path="bezierCurvePath"></el-amap-bezier-curve> -->
+            <el-amap-polygon
+                :path="polygonSetting.path"
+                :zIndex="polygonSetting.zIndex"
+                :strokeColor="polygonSetting.strokeColor"
+                :strokeWeight="polygonSetting.strokeWeight"
+                :strokeOpacity="polygonSetting.strokeOpacity"
+                :fillColor="polygonSetting.fillColor"
+                :fillOpacity="polygonSetting.fillOpacity"
+            ></el-amap-polygon>
+            <el-amap-text>
+                v-for="(item, index) in textMarkers" :position="item.position" :key="index" :text="item.text"
+                ></el-amap-text
+            >
+            <el-amap-marker :position="circleSetting.center"></el-amap-marker>
+            <el-amap-circle
+                :center="circleSetting.center"
+                :radius="circleSetting.radius"
+                :fillColor="circleSetting.fillColor"
+                :strokeColor="circleSetting.strokeColor"
+                :strokeWeight="circleSetting.strokeWeight"
+                :fillOpacity="circleSetting.fillOpacity"
+            ></el-amap-circle>
+        </el-amap>
     </div>
 </template>
 
 <script>
 export default {
-    components: {},
-    props: {},
+    name: 'demo',
     data() {
-        return {}
+        return {
+            reFresh: true,
+            zoom: 12,
+            // 地图的缩放大小，1是最大，能看到亚洲
+            zooms: [5, 15],
+            // 缩放区间
+            center: [116.3972282409668, 39.90960456049752],
+            // 经纬度，以这个为中心点
+            markers: [
+                { id: 'fgfd1', position: [116.3972282409668, 39.90960456049752], text: 'AAA' },
+                { id: 'fgfd2', position: [116.4072282409668, 39.92960456049752], text: 'BBB' },
+                { id: 'fgfd3', position: [116.382282409668, 39.94960456049752], text: 'CCC' },
+            ],
+            textMarkers: [
+                { position: [116.3972282409668, 39.90960456049752], text: 'AAA' },
+                { position: [116.4072282409668, 39.92960456049752], text: 'BBB' },
+                { position: [116.382282409668, 39.94960456049752], text: 'CCC' },
+            ],
+            bezierCurvePath: [
+                [116.3972282409668, 39.90960456049752],
+                [116.4072282409668, 39.92960456049752],
+            ],
+            polygonSetting: {
+                path: [
+                    [116.3972282409668, 39.90960456049752],
+                    [116.4072282409668, 39.92960456049752],
+                    [116.382282409668, 39.94960456049752],
+                    [116.342282409668, 39.91960456049752],
+                ],
+                strokeColor: '#FF33FF',
+                strokeWeight: 6,
+                strokeOpacity: 0.2,
+                fillOpacity: 0.4,
+                fillColor: '#1791fc',
+                zIndex: 50,
+            },
+            circleSetting: {
+                center: [116.39, 39.87], // 圆心位置
+                radius: 2000, // 圆半径
+                fillColor: '#1791fc', // 圆形填充颜色
+                strokeColor: '#fff', // 描边颜色
+                fillOpacity: 0.4,
+                strokeWeight: 2, // 描边宽度
+            },
+        }
     },
-    watch: {},
-    computed: {},
     methods: {},
-    created() {},
-    mounted() {},
+    mounted() {
+        console.log('123')
+    },
 }
 </script>
-<style scoped></style>
+<style scoped>
+.amap-demo {
+    width: 100%;
+    height: 800px;
+}
+/deep/ .amap-copyright,
+/deep/ .amap-logo {
+    display: none !important;
+}
+</style>
--- amap/src/main.js
+++ amap/src/main.js
@@ -1,34 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 // 引入vue-amap
-import AMap from 'vue-amap'
-Vue.use(AMap)
+import VueAMap from 'vue-amap'
 
-// 初始化vue-amap
-AMap.initAMapApiLoader({
-    // 申请的高德key
-    key: 'YOUR_KEY',
-    // 插件集合
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+    key: 'your amap key',
     plugin: [
         'AMap.Autocomplete',
-        //输入提示插件
         'AMap.PlaceSearch',
-        //POI搜索插件
         'AMap.Scale',
-        //右下角缩略图插件 比例尺
         'AMap.OverView',
-        //地图鹰眼插件
         'AMap.ToolBar',
-        //地图工具条
         'AMap.MapType',
-        //类别切换控件，实现默认图层与卫星图、实施交通图层之间切换的控制
         'AMap.PolyEditor',
-        //编辑 折线多，边形
         'AMap.CircleEditor',
-        //圆形编辑器插件
-        'AMap.Geolocation',
-        //定位控件，用来获取和展示用户主机所在的经纬度位置
     ],
+    // 默认高德 sdk 版本为 1.4.4
+    v: '1.4.4',
 })
 
 Vue.config.productionTip = false
