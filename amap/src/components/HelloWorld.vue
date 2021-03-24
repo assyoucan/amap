bb57a6e6b3bf31afbb8f9d2ffc4dbe986fdfb58b
chenmd <chen.mingda@cpe-smartcity.com>
2021-03-24 10:58:37 +0800

    123

--- amap/src/components/HelloWorld.vue
+++ amap/src/components/HelloWorld.vue
@@ -1,113 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <!-- <el-amap :center="center" id="amapcontainer"></el-amap> -->
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
+import { initMap, markerList } from "../config/mapConfig";
 export default {
-  name: 'HelloWorld',
-  data () {
+  components: {},
+  props: {},
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      map: "",
+      markers: [],
+      center: [112, 52],
+      currentPositionLat: "",
+      currentPositionLng: ""
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    initAmap() {
+      this.map = new AMap.Map("container", initMap);
+      this.map.plugin("AMap.Geolocation", () => {
+        let geolocation = new AMap.Geolocation({
+          enableHighAccuracy: true, //是否使用高精度定位，默认:true
+          timeout: 10000, //超过10秒后停止定位，默认：无穷大
+          maximumAge: 0, //定位结果缓存0毫秒，默认：0
+          convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+          showButton: true, //显示定位按钮，默认：true
+          buttonPosition: "LB", //定位按钮停靠位置，默认：'LB'，左下角
+          buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+          showMarker: true, //定位成功后在定位到的位置显示点标记，默认：true
+          showCircle: true, //定位成功后用圆圈表示定位精度范围，默认：true
+          panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
+          zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        });
+        this.map.addControl(geolocation);
+        geolocation.getCurrentPosition((status, result) => {
+          if (status === "complete") {
+            console.log("定位成功");
+            console.log(result);
+          } else {
+            console.log("定位失败");
+            console.log(result);
+          }
+        });
+        AMap.event.addListener(geolocation, "complete", postionInfo => {}); //返回定位信息
+        AMap.event.addListener(geolocation, "error", onError); //返回定位出错信息
+      });
+    },
+    createMarker() {}
+  },
+  created() {},
+  mounted() {
+    this.initAmap();
+    this.createMarker();
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+#amapcontainer {
+  width: 400px;
+  height: 400px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+#container {
+  width: 400px;
+  height: 400px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+/deep/ .amap-copyright,
+/deep/ .amap-logo {
+  display: none !important;
 }
 </style>
--- amap/src/main.js
+++ amap/src/main.js
@@ -1,15 +1,33 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import VueAMap from "vue-amap";
+//注册高德
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: "ad51a3da4424e26b37160efe14216b34", //这里是你的高德开发者key
+  plugin: [
+    "AMap.Autocomplete",
+    "AMap.PlaceSearch",
+    "AMap.Scale",
+    "AMap.OverView",
+    "AMap.ToolBar",
+    "AMap.MapType",
+    "AMap.PolyEditor",
+    "AMap.CircleEditor",
+    "AMap.Geolocation"
+  ],
+  v: "1.4.4"
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
